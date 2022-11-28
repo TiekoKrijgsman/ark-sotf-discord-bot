a17b9d3191b6de11cc4dea1aa2fa32b8617c0393
Divlo <contact@divlo.fr>
2022-11-28 08:48:42 +0100

    fix: increase invitations leaderboard size

--- src/commands/invitations-leaderboard.ts
+++ src/commands/invitations-leaderboard.ts
@@ -4,6 +4,8 @@ import { DiscordClient } from '../services/discord/DiscordClient.js'
 import type { DiscordCommand } from '../services/discord/DiscordCommand.js'
 import { pluralizeWord } from '../utils/pluralizeWord.js'
 
+const LEADERBOARD_SIZE = 10
+
 const invitationsLeaderboard: DiscordCommand = {
   data: new SlashCommandBuilder().setName('invitations-leaderboard').setDescription('Replies with the leaderboard of the members with the most invites.').toJSON(),
   execute: async (interaction) => {
@@ -13,11 +15,11 @@ const invitationsLeaderboard: DiscordCommand = {
     const sortedInvitationsLeaderboard = Array.from(usersInvitations.values()).sort((a, b) => {
       return b.invitationsCount - a.invitationsCount
     })
-    const leaderboard = sortedInvitationsLeaderboard.slice(0, 3).map((userInvitations, index) => {
+    const leaderboard = sortedInvitationsLeaderboard.slice(0, LEADERBOARD_SIZE).map((userInvitations, index) => {
       return `${index + 1}. ${userMention(userInvitations.inviter.id)}: ${bold(userInvitations.invitationsCount.toString())} ${pluralizeWord('member', userInvitations.invitationsCount)} invited.`
     })
     await interaction.reply({
-      content: `${underscore(bold('Invitations Leaderboard'))}\n\n${leaderboard.join('\n')}`,
+      content: `${underscore(bold(`Invitations Leaderboard - Top ${LEADERBOARD_SIZE}`))}\n\n${leaderboard.join('\n')}`,
       allowedMentions: {
         users: []
       }
